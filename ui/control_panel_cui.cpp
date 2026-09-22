788f8206c14adc67fa456674168540b28dc6286c
jame25 <jame25@users.noreply.github.com>
2026-09-22 10:14:29 +0100

    Update control_panel_cui.cpp

--- ui/control_panel_cui.cpp
+++ ui/control_panel_cui.cpp
@@ -145,9 +145,9 @@ void ControlPanelCUI::update_artwork() {
         // Try online via foo_artwork if enabled (may override stub)
         if (get_nowbar_online_artwork() && is_artwork_bridge_available()) {
             if (!m_tf_artist.is_valid())
-                titleformat_compiler::get()->compile_safe(m_tf_artist, "$if2(%foo_artwork_artist%,%artist%)");
+                titleformat_compiler::get()->compile_safe(m_tf_artist, "$if($or(%artist%,%title%),%artist%,%foo_artwork_artist%)");
             if (!m_tf_title.is_valid())
-                titleformat_compiler::get()->compile_safe(m_tf_title, "$if2(%foo_artwork_title%,%title%)");
+                titleformat_compiler::get()->compile_safe(m_tf_title, "$if($or(%artist%,%title%),%title%,%foo_artwork_title%)");
             pfc::string8 artist, title;
             if (is_playing_track) {
                 pc->playback_format_title(nullptr, artist, m_tf_artist, nullptr, playback_control::display_level_all);
